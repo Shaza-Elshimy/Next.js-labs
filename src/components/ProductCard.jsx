47f24c1d5925594d92238e787d0cbd0f1f1f4678
Shaza-Elshimy <shazahamdy011@gmail.com>
2026-06-19 22:26:26 +0300

    refactor: remove unnecessary anchor tag from product title in ProductCard component

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -9,10 +9,8 @@ const ProductCard = ({product}) => {
             <div class="mt-4 flex justify-between">
             <div>
                 <h3 class="text-sm text-gray-700">
-                <a href="#">
                     <span aria-hidden="true" class="absolute inset-0"></span>
                     {product.title}
-                </a>
                 </h3>
                 <p class="mt-1 text-sm text-gray-500">{product.category}</p>
             </div>
