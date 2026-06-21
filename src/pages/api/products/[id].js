068428ef7fc6de7bb3b732eacd9abb4aba6b7148
Shaza-Elshimy <shazahamdy011@gmail.com>
2026-06-21 21:32:57 +0300

    add update product api

--- src/pages/api/products/[id].js
+++ src/pages/api/products/[id].js
@@ -15,4 +15,15 @@ export default async function handler(req, res) {
     return res.status(200).json(product);
   }
 
+  if (req.method === "PUT") {
+
+  const product =
+    await Product.findByIdAndUpdate(
+      id,
+      req.body,
+      { new: true }
+    );
+
+  return res.status(200).json(product);
+}
 }
