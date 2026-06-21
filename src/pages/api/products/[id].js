f08443ca58e0b495d48ef4774e2379f397098953
Shaza-Elshimy <shazahamdy011@gmail.com>
2026-06-21 21:35:32 +0300

    add delete product api

--- src/pages/api/products/[id].js
+++ src/pages/api/products/[id].js
@@ -25,5 +25,15 @@ export default async function handler(req, res) {
     );
 
   return res.status(200).json(product);
-}
+  }
+
+  if (req.method === "DELETE") {
+
+    await Product.findByIdAndDelete(id);
+
+    return res.status(200).json({
+        message: "Deleted"
+    });
+
+  }
 }
